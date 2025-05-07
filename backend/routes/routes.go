0972e29849d46a0689acd7c179e67eb490d1364b
Harikrishnan-T <harikrishnan25082003@gmail.com>
2025-05-07 10:29:57 +0530

    Updated routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -9,4 +9,8 @@ import (
 func SetupRoutes(router *gin.Engine) {
 	router.POST("/api/save", controllers.SaveConversion)
 	router.GET("/api/files", controllers.GetFiles)
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "API is up!"})
+	})
 }
