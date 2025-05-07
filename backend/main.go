6892eb6f81364890ade1c1f1cb4e91282b7054ae
Harikrishnan-T <harikrishnan25082003@gmail.com>
2025-05-07 11:23:30 +0530

    Added cors

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"audio-converter/backend/models"
 	"audio-converter/backend/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,12 @@ func main() {
 	config.DB.AutoMigrate(&models.AudioFile{})
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowCredentials: true,
+	}))
 	routes.SetupRoutes(router)
 	router.Run(":8080")
 }
