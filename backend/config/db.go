68f37baff9aa47bec3c11556f44b6d0ee426f8db
Harikrishnan-T <harikrishnan25082003@gmail.com>
2025-05-07 10:18:11 +0530

    Update db.go to use DATABASE_URL from environment variables

--- backend/config/db.go
+++ backend/config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -9,11 +10,41 @@ import (
 
 var DB *gorm.DB
 
+// ConnectDB establishes a connection to the PostgreSQL database using the DATABASE_URL from environment variables
 func ConnectDB() {
-	dsn := "host=localhost user=postgres password=HariSatz dbname=audio_converter port=5432 sslmode=disable"
+	// Get the database URL from environment variables
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	// Connect to the PostgreSQL database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to DB:", err)
 	}
+
+	// Store the database connection for use elsewhere in the app
 	DB = db
+	log.Println("Database connected successfully")
 }
+
+// package config
+
+// import (
+// 	"log"
+
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func ConnectDB() {
+// 	dsn := "host=localhost user=postgres password=HariSatz dbname=audio_converter port=5432 sslmode=disable"
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatal("Failed to connect to DB:", err)
+// 	}
+// 	DB = db
+// }
